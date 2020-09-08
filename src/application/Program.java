6feb1a8b6a646e163ad661afc5ac91b1863433f3
marcoscaldas <donarumo@hotmail.com>
2020-09-08 16:21:26 -0300

    JDBC Alterar dados

---
+++ src/application/Program.java
@@ -0,0 +1,39 @@
+package application;     //  PROGRAMA PARA ALTERAR SALARIO DO VENDEDOR
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import db.DB;
+
+public class Program {
+
+	public static void main(String[] args)  {
+		
+		Connection conn = null;
+		PreparedStatement st =null;
+		try {
+			conn = DB.getConnection();
+			
+			st = conn.prepareStatement(
+					"UPDATE seller "  // espaços tambem contam para dar erros
+					+ "SET BaseSalary = BaseSalary + ? "
+					+ "WHERE "
+					+ "(DepartmentId = ?)");
+			
+			st.setDouble(1, 200.0);
+			st.setInt(2, 2);
+			
+			int rowsaffected = st.executeUpdate();
+			System.out.println("Done! Rows affected: " + rowsaffected );
+		}
+		
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+		}
+}
